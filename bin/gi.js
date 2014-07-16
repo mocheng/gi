16b9a5b3db8ee436590f24c390075c965981cc46
Morgan Cheng <morgan.chengmo@gmail.com>
2014-07-16 16:07:02 +0800

    fix #1: user can login with github credentials

--- bin/gi.js
+++ bin/gi.js
@@ -1,7 +1,28 @@
 #!/usr/bin/env node
 
-var user = require('../lib/user');
+var user = require('../lib/user'),
+  api = require('../lib/api'),
+  moment = require('moment');
 
-user.createUserAuth(function() {
-  console.log('done');
+user.createUserAuth(function(answers) {
+
+  api.authenticate({
+    type: 'basic',
+    username: answers.username,
+    password: answers.password
+  });
+
+  api.authorization.create({
+    scopes: ['user', 'public_repo', 'repo', 'repo:status', 'delete_repo', 'gist'],
+    note: 'gi (' + moment().format('MMMM YYYY, hh:mm:ss a') + ')', // this should be different for each request
+    note_url: 'https://github.com/mocheng/gi'
+  }, function(err, res) {
+    if (err) {
+      console.log('Fail to get Github auth: ' + err);
+      return;
+    }
+
+    console.log(res.token);
+
+  });
 });
